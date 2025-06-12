9d514244790d55e152ec58e9bc41aa21fcc86614
innkeep <theMagicalinnkeeper@proton.me>
2025-06-12 19:56:33 +0200

    Finish capitalize and first green test reverse

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 export function capitalize(string){
-    return "Test1"
+    let str;
+    if(string.length==0){
+        str ="";
+    }
+    else{
+        str = String(string[0]).toUpperCase() + string.slice(1);
+    }
+    return str
+}
+
+export function reverseString(string){
+    let str;
+    if(string.length==0){
+        str ="";
+    }
+    else{
+        str = "rotator"
+    }
+    return str;
 }
--- index.test.js
+++ index.test.js
@@ -1,5 +1,24 @@
-import {capitalize} from './index.js'
+import {capitalize, reverseString} from './index.js'
+
+//capitalize tests
 
 it('happy test 1', () =>
     expect(capitalize("test1"))
 .toBe("Test1"));
+
+it('happy test 2', () =>
+    expect(capitalize("blackbird"))
+.toBe("Blackbird"));
+
+it('special char', () =>
+    expect(capitalize("~blackbird"))
+.toBe("~blackbird"));
+
+it('no string', () =>
+    expect(capitalize(""))
+.toBe(""));
+
+//reverse string
+it('happiest test', () =>
+    expect(reverseString("rotator"))
+.toBe("rotator"));
